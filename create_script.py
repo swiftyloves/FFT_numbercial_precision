eff96bcfc811856245af56b9399b5a40676122ca
Jie-Wei <swiftywu@yahoo.com>
2017-12-13 08:35:54 -0500

    create all 1 script

--- create_script.py
+++ create_script.py
@@ -1,17 +1,20 @@
 
 
 inp = input('Enter the highest power of x: ')
-f = open('coefficient_polynomial' +inp+'.txt','w')
+amount = input('Enter the amount of x: ')
+f = open('coefficient_polynomial_all1_' +inp+'_'+amount+'.txt','w')
 
-power = int(input('Enter the power of 10 of coefficient: '));
+# power = int(input('Enter the power of 10 of coefficient: '));
 
 
-
-for i in range(int(inp)):
-    f.write(inp + "\n")
-    for j in range(int(inp)):
-        if i == j:
-          f.write(str(pow(10, power)) + " ")
-        else:
-          f.write("0 ")
-    f.write("\n")
+f.write(inp + "\n")
+for i in range(int(amount)):
+    if i < int(amount):
+        f.write("1 ")
+    else:
+        f.write("0 ")
+        # if i == j:
+        #   f.write(str(pow(10, power)) + " ")
+        # else:
+        #   f.write("0 ")
+    # f.write("\n")
